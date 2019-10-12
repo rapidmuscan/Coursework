820a11d570a400e6c91a690f6e3c5264c891a1c8
rapidmuscan <alsablino@yandex.ru>
2019-10-13 01:22:44 +0300

    Code was fixed and looks good))

--- Coursework/Coursework/Coursework.cpp
+++ Coursework/Coursework/Coursework.cpp
@@ -14,81 +14,42 @@ int check(int arr[arraysize][arraysize], int value, int curentr, int currentc)
 
 	vector<int> myvector;
 
-	for (int row = 0; row < arraysize; row++)
-	{
-		for (int column = 0; column < arraysize; column++)
-		{
+	for (int row = 0; row < arraysize; row++) 
+		for (int column = 0; column < arraysize; column++) 
 			myvector.push_back(arr[row][column]);
-		}
-	}
-	sort(myvector.begin(), myvector.end(), greater<int>());
-
-	for (int i = 0; i < curplace; i++) 
-	{
-		if (value == myvector[i])
-		{
-			return 0;
-		}
-	}
+	
+	for (int i = 0; i < curplace; i++) if (value == myvector[i]) return 0;
+	
 	return 1;
 }
 
 void Generator ()
 {
 	srand(time(0));
-
 	int arr[arraysize][arraysize];
-	int printtracker = 0;
-	
 	ofstream MyFile("text.txt");
-	
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			arr[i][j] = 1 + rand() % 19;
-			int m = 0;
-			while (m == 0)
-			{
-				if (check(arr, arr[i][j], i, j) == 1)
-				{
-					cout << check(arr, arr[i][j], i, j) << endl;
-					m = 1;
-				}
-				else
-				{
 
-					arr[i][j] = 1 + rand() % 19;
-				}
-			}
+	for (int i = 0; i < arraysize; i++)
+		for (int j = 0; j < arraysize; j++) {
+			arr[i][j] = rand() % 20;
+			
+			while (check(arr, arr[i][j], i, j) == 0) arr[i][j] = rand() % 20;
 			
 			MyFile << arr[i][j] << " ";
-			
-			if (j == 3)
-			{
-			
-				MyFile << endl;
-				printtracker++;
-			
-			}
+
+			if (j == 3) MyFile << endl;			
 		
 		}
-	}
-	
 	MyFile.close();
-
 }
-
-
-
-
 
 
 int main()
 {
 	int arr[arraysize][arraysize];
-	
+
 	Generator();
+
 	ifstream Myfile("text.txt");
 	for (int row = 0; row < arraysize; row++) {
 		for (int column = 0; column < arraysize; column++) {
@@ -98,8 +59,6 @@ int main()
 		cout << endl;
 	}
 	Myfile.close();
-
-
 
 	return 0;
 }
